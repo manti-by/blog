f1dd008ce3ffb44c63eaf8d3a3b3f2611b748aa1
Alexander Chaika <manti.by@gmail.com>
2022-07-20 21:57:16 +0300

    Add custom env var

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
+import logging
+from django.conf import settings
 from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
+    logger.info(f"MY_ENV_VAR: {settings.MY_ENV_VAR}")
     if request.GET.get("key") == "test":
         return HttpResponse("Posts with test key")
     return HttpResponse("Posts index view")
