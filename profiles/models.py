5f37d95d971ff419d74334b68dce4e39edb65ba6
Alexander Chaika <manti.by@gmail.com>
2022-07-29 20:54:12 +0300

    Add homework

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="addresses"
+    )
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
